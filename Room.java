13960d505d0d78cced2a376e96aab8d896898e3e
mitaka3 <justinbituin1207@gmail.com>
2023-10-21 00:26:38 +0800

    initial commit

---
+++ Room.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author user
+ */
+public class Room 
+{
+    public static void main(String[] args)
+    {
+        int length = 15, width = 25;
+        double area = length*width;
+        System.out.println("The Area of the room is " + area + " Square Feet." );
+    }  
+}
